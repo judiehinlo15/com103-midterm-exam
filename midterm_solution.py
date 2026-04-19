fb5ae4161b0eb21115a969c97d7bbb0ae0517147
Judie Hinlo <judie.hinlo@fcpc.edu.ph>
2026-04-19 22:42:20 +0800

    Update some changes in the code

--- midterm_solution.py
+++ midterm_solution.py
@@ -5,14 +5,15 @@ while True:
     else:
         break
 
+
 while True:
     weekly_budget = input("Enter your weekly budget: ").strip()
-
     if weekly_budget.replace('.', '', 1).isdigit():
         weekly_budget = float(weekly_budget)
         break
     else:
         print("Invalid input. Please enter a valid number.")
+
 
 categories = {
     1: "Food",
@@ -28,14 +29,17 @@ for key, value in categories.items():
 
 expenses = []
 
-for i in range(1, 5):
-    print(f"\n ---Expenses {i} ---")
 
+for i in range(1, 5):
+    print(f"\n--- Expense {i} ---")
+
+    
     while True:
-        try:
-            category_choice = int(input("Enter category number (1-5) or 0 to skip: "))
+        category_choice = input("Enter category number (1-5) or 0 to skip: ")
+        if category_choice.isdigit():
+            category_choice = int(category_choice)
             break
-        except ValueError:
+        else:
             print("Invalid input. Please enter a number.")
 
     if category_choice == 0:
@@ -46,19 +50,24 @@ for i in range(1, 5):
         print("Invalid category. Skipping this slot.")
         continue
 
+    
     description = input("Enter item description: ")
 
+    
     while True:
-        try:
-            amount = float(input("Enter amount spent: "))
+        amount = input("Enter amount spent: ")
+        if amount.replace('.', '', 1).isdigit():
+            amount = float(amount)
             break
-        except ValueError:
+        else:
             print("Invalid input. Please enter a number.")
 
+    
     alert = ""
     if amount > 0.25 * weekly_budget:
         alert = "! High Expense Alert!"
 
+    # Save expense
     expenses.append({
         "category": categories[category_choice],
         "description": description,
@@ -66,22 +75,24 @@ for i in range(1, 5):
         "alert": alert
     })
 
+
 total_spent = sum(item["amount"] for item in expenses)
 remaining_balance = weekly_budget - total_spent
 
+
 print(f"\n{student_name} ===== Expenses Log =====")
-print(f"Weekly Budget: {weekly_budget:.2f}\n")
+print(f"Weekly Budget: ₱{weekly_budget:,.2f}\n")
 
 print("Expenses:")
 if not expenses:
     print("No expenses recorded.")
 else:
     for item in expenses:
-        print(f"- {item['category']} | {item['description']} | {item['amount']:.2f} {item['alert']}")
+        print(f"- {item['category']} | {item['description']} | ₱{item['amount']:,.2f} {item['alert']}")
 
 print("=" * 40)
-print(f"\nTotal Spent: {total_spent:.2f}")
-print(f"Remaining Balance: {remaining_balance:.2f}")
+print(f"\nTotal Spent: ₱{total_spent:,.2f}")
+print(f"Remaining Balance: ₱{remaining_balance:,.2f}")
 
 if remaining_balance >= 0:
     print("Status: Budget OK! Keep it up.")
